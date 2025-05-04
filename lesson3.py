46747aa600d145f069fb6e52540a0a3c7cf78f27
Arrtem391 <kudinowartem22@gmail.com>
2025-05-04 14:10:26 +0300

    Add files via upload

---
+++ lesson3.py
@@ -0,0 +1,43 @@
+import smtplib
+#import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+login = os.getenv('login_yandex')
+password = os.getenv('password_yandex')
+server = smtplib.SMTP_SSL('smtp.yandex.ru', 465)
+server.login(login, password)
+
+site = 'https://dvmn.org/profession-ref-program/spytf/1WJiV/'
+friend_name = 'Илюха'
+my_name = 'Артём'
+
+letter = """From: devmanorg@yandex.ru
+To: kudinowartem22@gmail.com
+Subject: Приглашение!
+Content-Type: text/plain; charset="UTF-8";
+
+Привет, %friend_name%! %my_name% приглашает тебя на сайт %website%!
+
+%website% — это новая версия онлайн-курса по программированию. 
+Изучаем Python и не только. Решаем задачи. Получаем ревью от преподавателя. 
+
+Как будет проходить ваше обучение на %website%? 
+
+→ Попрактикуешься на реальных кейсах. 
+Задачи от тимлидов со стажем от 10 лет в программировании.
+→ Будешь учиться без стресса и бессонных ночей. 
+Задачи не «сгорят» и не уйдут к другому. Занимайся в удобное время и ровно столько, сколько можешь.
+→ Подготовишь крепкое резюме.
+Все проекты — они же решение наших задачек — можно разместить на твоём GitHub. Работодатели такое оценят. 
+
+Регистрируйся → %website%  
+На курсы, которые еще не вышли, можно подписаться и получить уведомление о релизе сразу на имейл."""
+
+letter = letter.replace('%website%', site).replace('%friend_name%', friend_name).replace('%my_name%', my_name)
+
+server.sendmail(login, 'kudinowartem22@gmail.com', letter.encode('utf-8'))
+server.quit()
+
+print(letter)
